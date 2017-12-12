0c3484a1540b5f4334b93676f2d2c8c6f033566f
MarcoCarneiro <1160777@isep.ipp.pt>
2017-12-12 11:48:17 +0000

    [Pom.xml]JavaToHtml dependency added.

---
+++ src/main/java/lapr/project/utils/HTMLExporter.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lapr.project.utils;
+
+/**
+ *
+ * @author Utilizador
+ */
+public class HTMLExporter {
+    
+    /**
+     * HTML Exporter
+     */
+    public HTMLExporter(){
+        
+    }
+    
+}
