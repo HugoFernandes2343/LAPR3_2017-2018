14ad89684508841760f75fab8c55f6fe3b84e3b8
Filipe Simao <1161213@isep.ipp.pt>
2017-12-13 15:34:09 +0000

    [LP31718G42-22][Controller Implementation][UC01] completed

--- src/main/java/lapr/project/controller/CreateProjectController.java
+++ src/main/java/lapr/project/controller/CreateProjectController.java
@@ -5,8 +5,12 @@
  */
 package lapr.project.controller;
 
+import java.io.IOException;
 import lapr.project.model.Project;
+import lapr.project.model.RoadNetwork;
 import lapr.project.model.TravelByPhysics;
+import lapr.project.model.VehicleList;
+import lapr.project.utils.FileXML;
 
 /**
  *
@@ -37,19 +41,37 @@ public class CreateProjectController {
      *
      * @param name - name introduced by the user.
      * @param description - description introduced by the user.
+     * @return true if the new project is created, false otherwise.
      */
-    public void createProject(String name, String description) {
+    public boolean createProject(String name, String description) {
+        if(name.isEmpty() || description.isEmpty()){
+            return false;
+        }
+        
         Project p = new Project();
         p.setName(name);
         p.setDescription(description);
         this.newP = p;
+        return true;
     }
 
     /**
      * Method that reads all the information needed for the new project.
+     * @param fileRoadNetwork - path to the file with the road network.
+     * @param fileVehicleList - path to the file with the vehicle list.
+     * @return 
      */
-    public void readInfo() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public boolean readInfo(String fileRoadNetwork, String fileVehicleList) {
+        
+        try {
+            RoadNetwork r = FileXML.loadXmlRoadNetwork(fileRoadNetwork);
+            VehicleList v = FileXML.loadXmlVehicleList(fileVehicleList);
+            this.newP.setRoadNetwork(r);
+            this.newP.setVehicleList(v);
+            return true;
+        } catch (IOException ex) {
+            return false;
+        }
     }
 
     /**
