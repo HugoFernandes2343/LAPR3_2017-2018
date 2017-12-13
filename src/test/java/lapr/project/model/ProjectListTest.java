59a4e326823860f540cf399d1502de8dbb33e9b2
Hugo Fernandes <1161155@isep.ipp.pt>
2017-12-13 11:18:00 +0000

    [LP31718G42-26][Model Testing][UC02] Tests of the UC02 Select Project

--- src/test/java/lapr/project/model/ProjectListTest.java
+++ src/test/java/lapr/project/model/ProjectListTest.java
@@ -5,10 +5,15 @@
  */
 package lapr.project.model;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
+import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.BeforeClass;
 
 /**
  *
@@ -20,6 +25,22 @@ public class ProjectListTest {
 
     public ProjectListTest() {
         instance = new ProjectList();
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
     }
 
     /**
@@ -53,5 +74,46 @@ public class ProjectListTest {
         Project actualProject = new Project();
         instance.setActualProject(actualProject);
         assertEquals(actualProject, instance.getActualProject());
+    }
+
+    /**
+     * Test of getAllNames method, of class ProjectList.
+     */
+    @Test
+    public void testGetAllNames() {
+        System.out.println("getAllNames");
+        ProjectList instance = new ProjectList();
+        Project p1 = new Project();
+        Project p2 = new Project();
+        p1.setName("Proj1");
+        p2.setName("Proj2");
+        instance.addProject(p1);
+        instance.addProject(p2);
+
+        ArrayList<String> expResult = new ArrayList<>();
+        expResult.add("Proj1");
+        expResult.add("Proj2");
+        ArrayList<String> result = instance.getAllNames();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getProject method, of class ProjectList.
+     */
+    @Test
+    public void testGetProject() {
+        System.out.println("getProject");
+        String name = "Proj1";
+        ProjectList instance = new ProjectList();
+        Project p1 = new Project();
+        Project p2 = new Project();
+        p1.setName("Proj1");
+        p2.setName("Proj2");
+        instance.addProject(p1);
+        instance.addProject(p2);
+        Project expResult = p1;
+        Project result = instance.getProject(name);
+        assertEquals(expResult, result);
+        
     }
 }
